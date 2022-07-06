542030c0cdd35a6cb9466e29d99e74db0bc2a5b9
Billy Lando <billy_lando@yahoo.com>
2022-07-06 13:10:06 +0200

    feat: init nx workspace with nest

---
+++ apps/ddd-workshop/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'ddd-workshop',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/ddd-workshop',
+};
---
+++ apps/ddd-workshop/src/app/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getData(): { message: string } {
+    return { message: 'Welcome to ddd-workshop!' };
+  }
+
+  getHelloWorld(): string {
+    return 'Hello World!';
+  }
+}
